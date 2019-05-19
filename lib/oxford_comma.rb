50ca1a08ab0a4ea6d0ab58ae813f39569d998e95
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 18:42:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     string = array.join(" and ")
   elsif array.size >= 3
+    second_last = array[-2]
     string = array[0..].join(", ") + (", and ") + array.last
   else
     return "invalid input"
