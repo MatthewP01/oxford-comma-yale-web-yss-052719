5bc6b1bfc65dda1a0d681638fd901e013e90ef51
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 18:39:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     string = array.join(" and ")
   elsif array.size == 3
-    string = array.join(" and ") + (", and") + array[-1]
+    string = array.join(" and ") + (", and") + array.last
   end
 
 end
